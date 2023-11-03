c594b4065f9cffad411395517f931dee3d5d690c
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-03 10:54:03 +0600

    router setup completed

---
+++ src/Router/route.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import Login from "./../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import MyCart from "../pages/MyCart/MyCart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/myCart",
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
+
+export default router;
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,18 @@
+import { createContext } from "react";
+import PropTypes from "prop-types";
 
+export const AuthContext = createContext(null);
 
+const AuthProvider = ({ children }) => {
+  const authInfo = {};
 
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+AuthProvider.propTypes = {
+  children: PropTypes.node,
 };
 
 export default AuthProvider;
