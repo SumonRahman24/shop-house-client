378ca5660ba3c85c42a229db5763b838a739351c
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-03 11:42:46 +0600

    private route setup completed

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,60 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import PropTypes from "prop-types";
+import auth from "./../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // signUp
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(email, password);
+  };
+
+  // signIn
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(email, password);
+  };
+
+  // logOut
+  const logOut = () => {
+    setLoading(true);
+    return logOut(auth);
+  };
+
+  // signInWtihGoogle
+  const signInWithGoogle = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(email, password);
+  };
+
+  // onAuthStateChanged
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoading(true);
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    if (user) {
+      setLoading(false);
+    }
+
+    return () => {
+      unsubscribe();
+    };
+  }, [user]);
+
+  const authInfo = { signIn, signUp, logOut, loading, signInWithGoogle };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
